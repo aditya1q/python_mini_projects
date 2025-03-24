c6ffefab1b75e53e986cd5acacf20c0c5f72ee4e
aditya1q <kanhatiwari9410@gmail.com>
2025-03-24 16:41:35 +0530

    implementing tic tac toe game

--- digital_clock/digital_clock.py
+++ digital_clock/digital_clock.py
@@ -1,6 +1,19 @@
-from time import strftime
+# from time import strftime, strptime,localtime
 
-get_time = strftime('%H:%M:%S %p') # for time
-get_date = strftime('%D') # for date
-print(get_time)
-print(get_date)
+# get_time = strftime('%H:%M:%S %p') # for time
+# get_date = strftime('%D') # for date
+# print(get_time)
+# print(get_date)
+
+
+
+# epoch_time = 1742361097
+# get_strf_time = strftime('%H:%M:%S %Y', localtime(epoch_time))
+# print(get_strf_time)
+
+
+# time_string = "14:30:25 2023"
+# # Parse the string into a time tuple using strptime
+# get_strp_time = strptime(time_string, "%H:%M:%S %Y")
+# # Print the struct time
+# print(get_strp_time)
---
+++ tic_tac_toe/beginner.py
@@ -0,0 +1,6 @@
+def printBoard():
+    pass
+
+
+if __name__ == '__main__':
+    print('welcome to tic tac toe')
